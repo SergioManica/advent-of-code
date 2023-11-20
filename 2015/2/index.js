1ca09079a138d828779318cd12d10a7ec87bb3c5
sergio_manica <serginhomanica@hotmail.com>
2023-11-20 16:59:31 -0300

    feat: first possible solution

--- 2015/2/index.js
+++ 2015/2/index.js
@@ -3,29 +3,50 @@ const fs = require("fs");
 
 const filePath = "./2015/2/input.txt";
 
-fs.readFile(filePath,"utf8", (err,inputData)=>{
-    if (err) {
-        console.error("Error reading the file:", err);
-    }else{
-        calculatePaper(inputData)
-    }
-})
+fs.readFile(filePath, "utf8", (err, inputData) => {
+  if (err) {
+    console.error("Error reading the file:", err);
+  } else {
+    calculatePaper(inputData);
+  }
+});
 
-    function calculatePaper(data){
-        array = []
-        array.push(data)
-        const arrayOfPresents = array[0].split("\n")
-        // console.log(arrayOfPresents);
-        arraySeparatedByDimentions = []
-        // const arraySeparatedByDimentions = arrayOfPresents.map((item)=>{
-        //     item.split("x")
-        // })
+function calculatePaper(data) {
+  let length = "";
+  let width = "";
+  let height = "";
+  array = [];
+  array.push(data);
+  const arrayOfPresents = array[0].split("\n");
 
-        
-        arrayOfPresents.forEach(element => {
-            element.split("x")
-            let onlyDimentions = element.split("x")
-            arraySeparatedByDimentions.push(onlyDimentions)
-        });
-        console.log(arraySeparatedByDimentions[0]);
+  let sum = 0;
+
+  arrayOfPresents.forEach((element) => {
+    let onlyDimentions = element.split("x");
+    length = onlyDimentions[0];
+    width = onlyDimentions[1];
+    height = onlyDimentions[2];
+
+    let l = 2 * length * width;
+    let w = 2 * width * height;
+    let h = 2 * height * length;
+    let smallestSide;
+    let surfaceAreaOfPresents;
+    if (l <= w && l <= h) {
+      smallestSide = l;
+      surfaceAreaOfPresents = l + w + h + smallestSide;
     }
+    if (w <= l && w <= h) {
+      smallestSide = w;
+      surfaceAreaOfPresents = l + w + h + smallestSide;
+    }
+    if (h <= l && h <= w) {
+      smallestSide = h;
+      surfaceAreaOfPresents = l + w + h + smallestSide;
+    }
+    sum += surfaceAreaOfPresents;
+  });
+  console.log(sum);
+}
+
+// FORMULA -> 2*l*w + 2*w*h + 2*h*l +
